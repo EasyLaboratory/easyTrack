2c914a965721ae73e1d856f851f898ee94815163
XingjieFu <fuxingjie1998@163.com>
2024-09-29 11:18:42 +0800

    update planner and msgs

--- airsim_planning.sh
+++ airsim_planning.sh
@@ -3,10 +3,10 @@
 # 在同一个终端中打开三个标签页，并分别执行指令
 
 # 打开第一个标签页并执行第一个指令
-gnome-terminal --tab --title="rviz" -- bash -c "source devel/setup.bash; roslaunch mapping rviz_sim.launch; exec bash"
+gnome-terminal --tab --title="RVIZ" -- bash -c "source devel/setup.bash; roslaunch mapping rviz_sim.launch; exec bash"
 
 # 等待3秒
 sleep 3
 
 # 打开第三个标签页并执行第二个指令
-gnome-terminal --tab --title="simulation_airsim" -- bash -c "source devel/setup.bash; roslaunch planning simulation_airsim.launch > >(tee ~/simulation_airsim.log) 2> >(tee -a ~/simulation_airsim.log >&2); exec bash"
+gnome-terminal --tab --title="SIMULATION_AIRSIM" -- bash -c "source devel/setup.bash; roslaunch planning simulation_airsim.launch > >(tee ~/simulation_airsim.log) 2> >(tee -a ~/simulation_airsim.log >&2); exec bash"
