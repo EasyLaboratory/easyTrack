8812c83ae340a09b059735b7d7f57d6c4b4aa3bc
XingjieFu <fuxingjie1998@163.com>
2025-02-19 23:04:17 +0800

    update pp and test.sh

--- test.sh
+++ test.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-# 执行Block.sh脚本
-gnome-terminal -- bash -c "cd /home/fxj/5app/water_fluid_linux_shipping/LinuxNoEditor; bash Blocks.sh; exec bash"
+# 第一个终端标签
+gnome-terminal --tab --title="Tab1" -e "bash -c 'cd /home/fuxingjie/4app/water_fluid_linux_shipping_2/LinuxNoEditor; bash Blocks.sh; exec bash'"
 
-# 等待3秒
+# 等待3秒后打开第二个标签
 sleep 3
+gnome-terminal --tab --title="Tab2" -e "bash -c 'cd /home/fuxingjie/1code/easyTrack; source ./devel/setup.bash; roslaunch se3controller se3controller.launch; exec bash'"
 
-# 切换到easyTrack目录并运行roslaunch
-gnome-terminal -- bash -c "cd /home/fxj/1code/easyTrack; source ./devel/setup.bash; roslaunch se3controller se3controller.launch; exec bash"
+# 再等待1秒后打开第三个标签
 sleep 1
-
-# 切换到easyTrack目录并运行roslaunch
-gnome-terminal -- bash -c "cd /home/fxj/1code/easyTrack; source ./devel/setup.bash; roslaunch predictor pp.launch; exec bash"
-
+gnome-terminal --tab --title="Tab3" -e "bash -c 'cd /home/fuxingjie/1code/easyTrack; source ./devel/setup.bash; roslaunch predictor pp.launch; exec bash'"
