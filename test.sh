aa3521d3b874b1484ae17d7009e171f389223af5
XingjieFu <fuxingjie1998@163.com>
2025-02-26 17:04:11 +0800

    bad ok version

--- test.sh
+++ test.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 # 第一个终端标签
-gnome-terminal --tab --title="Tab1" -e "bash -c 'cd /home/fuxingjie/4app/water_fluid_linux_shipping_2/LinuxNoEditor; bash Blocks.sh; exec bash'"
+# gnome-terminal --tab --title="Tab1" -e "bash -c 'cd /home/fxj/5app/water_fluid_linux_shipping_3/LinuxNoEditor; bash Blocks.sh; exec bash'"
 
+gnome-terminal --tab --title="Tab2" -e "bash -c 'cd /home/fxj/1code/easyTrack; source ./devel/setup.bash; roslaunch perception sensor.launch; exec bash'"
 # 等待3秒后打开第二个标签
-sleep 3
-gnome-terminal --tab --title="Tab2" -e "bash -c 'cd /home/fuxingjie/1code/easyTrack; source ./devel/setup.bash; roslaunch se3controller se3controller.launch; exec bash'"
+# sleep 5
+# gnome-terminal --tab --title="Tab2" -e "bash -c 'cd /home/fxj/1code/easyTrack; source ./devel/setup.bash; roslaunch se3controller se3controller.launch; exec bash'"
 
 # 再等待1秒后打开第三个标签
-sleep 1
-gnome-terminal --tab --title="Tab3" -e "bash -c 'cd /home/fuxingjie/1code/easyTrack; source ./devel/setup.bash; roslaunch predictor pp.launch; exec bash'"
+sleep 5
+gnome-terminal --tab --title="Tab3" -e "bash -c 'cd /home/fxj/1code/easyTrack; source ./devel/setup.bash; roslaunch predictor pp.launch; exec bash'"
