561ff5c551ec8d93df3056f88cd12e4b96c03280
XingjieFu <fuxingjie1998@163.com>
2025-04-11 15:35:03 +0800

    update cmd2airsim and perception and pp and test.sh

--- test.sh
+++ test.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
 # 第一个终端标签
-gnome-terminal --tab --title="Tab1" -e "bash -c 'cd /home/ubuntu/3software/water_fluid_linux_shipping_3/LinuxNoEditor; bash Blocks.sh; exec bash'"
+# gnome-terminal --tab --title="Tab1" -e "bash -c 'cd /home/ubuntu/3software/water_fluid_linux_shipping_3/LinuxNoEditor; bash Blocks.sh; exec bash'"
 
-gnome-terminal --tab --title="Tab2" -e "bash -c 'cd /home/fxj/1code/easyTrack; source ./devel/setup.bash; roslaunch perception sensor.launch; exec bash'"
-# 等待3秒后打开第二个标签
+# gnome-terminal --tab --title="Tab2" -e "bash -c 'cd /home/fxj/1code/easyTrack; source ./devel/setup.bash; roslaunch perception sensor.launch; exec bash'"
+
+# 打开第二个标签
 # sleep 5
-# gnome-terminal --tab --title="Tab2" -e "bash -c 'cd /home/fxj/1code/easyTrack; source ./devel/setup.bash; roslaunch se3controller se3controller.launch; exec bash'"
+gnome-terminal --tab --title="Tab2" -e "bash -c 'cd /home/ubuntu/1code/easyTrack; source ./devel/setup.bash; roslaunch se3controller se3controller.launch; exec bash'"
 
-# 再等待1秒后打开第三个标签
+# 打开第三个标签
+sleep 4
+gnome-terminal --tab --title="Tab3" -e "bash -c 'cd /home/ubuntu/1code/easyTrack; source ./devel/setup.bash; roslaunch planner plan_ros.launch; exec bash'"
+
+# 打开第四个标签
 sleep 2
-gnome-terminal --tab --title="Tab3" -e "bash -c 'cd /home/ubuntu/1code/easyTrack; source ./devel/setup.bash; roslaunch predictor pp.launch; exec bash'"
+gnome-terminal --tab --title="Tab4" -e "bash -c 'cd /home/ubuntu/1code/easyTrack; source ./devel/setup.bash; roslaunch predictor predict_ros.launch; exec bash'"
